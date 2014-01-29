2bb506b1da89423ac77f47b8407a350f4b254814
AugTech <info@aug-tech.co.uk>
2014-01-29 10:29:56 +0000

    Add test cases to class to simplify test apps

--- GeoPackage_Lib/com/augtech/geoapi/geopackage/GpkgTEST.java
+++ GeoPackage_Lib/com/augtech/geoapi/geopackage/GpkgTEST.java
@@ -16,7 +16,7 @@
 package com.augtech.geoapi.geopackage;
 
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,10 +25,13 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.AttributeType;
 import org.opengis.feature.type.GeometryType;
 import org.opengis.feature.type.Name;
+import org.opengis.geometry.BoundingBox;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import com.augtech.geoapi.feature.FeatureCollection;
 import com.augtech.geoapi.feature.NameImpl;
@@ -38,9 +41,12 @@ import com.augtech.geoapi.feature.type.AttributeTypeImpl;
 import com.augtech.geoapi.feature.type.GeometryDescriptorImpl;
 import com.augtech.geoapi.feature.type.GeometryTypeImpl;
 import com.augtech.geoapi.feature.type.SimpleFeatureTypeImpl;
+import com.augtech.geoapi.geometry.BoundingBoxImpl;
+import com.augtech.geoapi.geopackage.geometry.StandardGeometryDecoder;
 import com.augtech.geoapi.geopackage.table.FeaturesTable;
 import com.augtech.geoapi.geopackage.table.TilesTable;
 import com.augtech.geoapi.referncing.CoordinateReferenceSystemImpl;
+import com.augtech.geoapi.utils.DataUtilities;
 import com.vividsolutions.jts.geom.Geometry;
 
 /** A test layer that utilises some of the Aug-Tech GeoAPI facilities 
@@ -56,6 +62,13 @@ public class GpkgTEST {
 	ISQLDatabase gpkgDatabase = null;
 	Logger log = Logger.getAnonymousLogger();
 	static String TEST_GML_FILE = "test_gml";
+	
+	public static final int TEST_HAITI = 0;
+	public static final int TEST_GML = 1;
+	public static final int TEST_NZ = 2;
+	public static final int TEST_TILES = 3;
+	public static final int TEST_NZ_DEM = 4;
+	public static final int TEST_SIGMA = 5;
 	
 	/**
 	 * 
@@ -147,7 +160,7 @@ public class GpkgTEST {
 			// Load each file..
 			for (File f : path.listFiles()) {
 				loader.setInitialID( f.toString() );
-				numLoaded = loader.loadFeatures( new FileInputStream(f) );
+				numLoaded = loader.loadFeatures( f );
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -171,15 +184,20 @@ public class GpkgTEST {
 	 */
 	public boolean loadGMLToCollection(boolean overwrite) throws Exception {
 		
-		InputStream gmlFile = this.getClass().getResourceAsStream(TEST_GML_FILE);
-		if (gmlFile==null) return false;
+		InputStream gmlRes = this.getClass().getResourceAsStream(TEST_GML_FILE);
+		File gmlFile = new File(System.getProperty("java.io.tmpdir"), "tmpGml.gml");
+		if (gmlFile.exists()) gmlFile.delete();
+		DataUtilities.streamCopy(gmlRes, new FileOutputStream(gmlFile));
+		
+		if (!gmlFile.exists()) return false;
 		
 		log.log(Level.INFO, "Loading GML...");
 
 		GML2_1 gmlLoader = new GML2_1(null);
 		
 		int numFeat = gmlLoader.loadFeatures( gmlFile );
-
+		gmlFile.delete();
+		
 		Map<Name, SimpleFeatureType> fTypes = gmlLoader.getLoadedTypes();
 		if (overwrite) {
 			featureCollection = new FeatureCollection();
@@ -259,7 +277,6 @@ public class GpkgTEST {
 	public boolean createFeatureTablesFromCollection(boolean allTypes) throws Exception {
 		if (featureCollection==null) loadGMLToCollection(true);
 		
-		boolean processed = false;
 		log.log(Level.INFO, "Creating feature tables...");
 		
 		int numCreated = 0;
@@ -282,13 +299,178 @@ public class GpkgTEST {
 				break;
 			}
 
-			geoPackage.createFeaturesTable(single, featureCollection.getBounds() );
+			FeaturesTable ft = geoPackage.createFeaturesTable(single, featureCollection.getBounds() );
 			
+			if (ft==null) return false;
 			numCreated++;
 		}
 		
 		log.log(Level.INFO, "Created "+numCreated+" feature tables (types)");
 		
-		return processed;
+		return true;
+	}
+	/**
+	 * 
+	 * @param testCase
+	 * @param fb
+	 * @return
+	 * @throws Exception
+	 */
+	public boolean runQeryTestCase(int testCase, ITestFeedback fb) throws Exception {
+		
+		if (geoPackage==null) {
+			setTestMsg(fb, "No GeoPackage connected");
+			return false;
+		}
+		
+		List<SimpleFeature> feats = null;
+		BoundingBox bbox = null;
+		CoordinateReferenceSystem crs = null;
+		
+		switch (testCase) {
+
+		case TEST_HAITI:// Port-au-Prince, Haiti (from Compusult)
+			
+			crs = new CoordinateReferenceSystemImpl("4326");
+			
+			feats = geoPackage.getFeatures("linear_features", "id=1", new StandardGeometryDecoder() );
+			setTestMsg(fb, feats.size()+" linear_feature read by query");
+			
+			bbox = new BoundingBoxImpl(-72.335822, -72.633677, 18.4617532, 18.8551624, crs);
+			feats = geoPackage.getFeatures("linear_features", bbox);
+			setTestMsg(fb, feats.size()+" linear_features read via bbox query!");
+
+			break;
+
+		case TEST_GML:
+			
+			crs = new CoordinateReferenceSystemImpl("4326");
+			bbox = new BoundingBoxImpl(-2.14, -2.14, 52.2, 52.3, crs);
+			
+			// Get all features from a table
+			feats = geoPackage.getFeatures("surface_water_sewer", "", new StandardGeometryDecoder());
+			setTestMsg(fb, feats.size()+" surface_water_sewer feature(s) read.");
+
+			// Get all features within a bounding box
+			feats = geoPackage.getFeatures("surface_water_sewer", bbox);
+			setTestMsg(fb, feats.size()+" surface_water_sewer feature(s) read via bbox query!");
+
+			feats = geoPackage.getFeatures("foul_sewer", bbox);
+			setTestMsg(fb, feats.size()+" foul_sewer feature(s) read via bbox query!");
+
+			break;
+
+		case TEST_TILES:
+			
+			crs = new CoordinateReferenceSystemImpl("3857");
+			bbox = new BoundingBoxImpl(-239247.301401622, -238788.67923184743, 6846158.279178806, 6846616.90134858,crs);
+			
+			// Get all tiles from a single table
+			feats = geoPackage.getTiles("GoogleMaps", null);
+			setTestMsg(fb, feats.size()+" images(s) (all) read from GoogleMaps");
+
+			// Get a single tile we know we have
+			feats = geoPackage.getTiles("GoogleMaps", "zoom_level=18 and tile_column=129506 and tile_row=86286");
+			setTestMsg(fb, feats.size()+" images(s) read direct by query");
+
+			// Get a set of tiles within/ across a bounding box
+			feats = geoPackage.getTiles("GoogleMaps", bbox, 18);
+			setTestMsg(fb, feats.size()+" images(s) read by BBOX query");
+
+			break;
+
+		case TEST_NZ:
+			crs = new CoordinateReferenceSystemImpl("27200");
+			bbox = new BoundingBoxImpl(2560000, 2565000, 6600000, 6605000, crs);
+			
+			feats = geoPackage.getFeatures("nzlri-vegetation", bbox);
+			setTestMsg(fb, feats.size()+" feature(s) read from [nzlri-vegetation] by BBOX query");
+			
+			feats = geoPackage.getFeatures("lcdb-v33-land-cover-datab", bbox);
+			setTestMsg(fb, feats.size()+" feature(s) read from [lcdb-v33-land-cover-datab] by BBOX query");
+			
+			//bbox = ProjectionUtils.reproject(bbox, "4326");
+			crs = new CoordinateReferenceSystemImpl("4326");
+			bbox = new BoundingBoxImpl(173.54606, 173.60088, -35.8007, -35.75527, crs);
+			
+			feats = geoPackage.getFeatures("nzlri-north-island-edition", bbox);
+			setTestMsg(fb, feats.size()+" feature(s) read from [north-island-edition] by BBOX query");
+
+			feats = geoPackage.getFeatures("s-map-a-new-soil-spatial", bbox);
+			setTestMsg(fb, feats.size()+" feature(s) read from [new-soil-spatial] by BBOX query");
+			
+			break;
+			
+		case TEST_NZ_DEM:
+			
+			crs = new CoordinateReferenceSystemImpl("27200");
+			bbox = new BoundingBoxImpl(2560000, 2565000, 6600000, 6605000, crs);
+
+			feats = geoPackage.getFeatures("TerrainPoint", bbox);
+			setTestMsg(fb, feats.size()+" feature(s) read from [terrainpoint] by BBOX query");
+			
+			break;
+		
+		case TEST_SIGMA:
+			crs = new CoordinateReferenceSystemImpl("3857");
+			bbox = new BoundingBoxImpl(-20037508.3428, 20037508.3428, -20037471.2051, 20037471.2051, crs);
+
+			String table = "fromosm_tiles";
+			
+			// Get all tiles from a single table
+			feats = geoPackage.getTiles(table, null);
+			setTestMsg(fb, feats.size()+" images(s) (all) read from "+table);
+
+			// Get a single tile we know we have
+			feats = geoPackage.getTiles(table, "zoom_level=10 and tile_column=341 and tile_row=479");
+			setTestMsg(fb, feats.size()+" images(s) read direct by query from "+table);
+
+			// Get a set of tiles within/ across a bounding box
+			feats = geoPackage.getTiles(table, bbox, 10);
+			setTestMsg(fb, feats.size()+" images(s) read by BBOX query from "+table);
+			
+			// Vector...
+			crs = new CoordinateReferenceSystemImpl("4326");
+			bbox = new BoundingBoxImpl(-73, -71, 17.5, 19.1, crs);
+			table = "geonames";
+			
+			// Get all features from a table
+			feats = geoPackage.getFeatures(table, "", new StandardGeometryDecoder());
+			setTestMsg(fb, feats.size()+" "+table+" feature(s) read.");
+
+			// Get all features within a bounding box
+			feats = geoPackage.getFeatures(table, bbox);
+			setTestMsg(fb, feats.size()+" "+table+" feature(s) read via bbox query!");
+
+			feats = geoPackage.getFeatures(table, bbox);
+			setTestMsg(fb, feats.size()+" "+table+" feature(s) read via bbox query!");
+			
+			break;
+		}
+		
+		return true;
+		
+	}
+	/**
+	 * 
+	 * @param fb
+	 * @param msg
+	 */
+	private void setTestMsg(ITestFeedback fb, String msg) {
+		if (fb!=null) {
+			fb.testComplete( msg );
+		} else {
+			log.log(Level.INFO, msg);
+		}
+	}
+	/** Simple interface to receive a message from load and query tests.
+	 *
+	 */
+	public interface ITestFeedback {
+		/** A sets has been completed with the supplied text
+		 * 
+		 * @param msg
+		 */
+		public void testComplete(String msg);
 	}
 }
