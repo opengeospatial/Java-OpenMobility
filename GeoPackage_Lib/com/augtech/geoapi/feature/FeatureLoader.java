0578418d9304c0d737df693567db06b36e0bb454
AugTech <info@aug-tech.co.uk>
2014-01-29 10:29:33 +0000

    Direct insertion to GeoPackage added

--- GeoPackage_Lib/com/augtech/geoapi/feature/FeatureLoader.java
+++ GeoPackage_Lib/com/augtech/geoapi/feature/FeatureLoader.java
@@ -15,14 +15,21 @@
  */
 package com.augtech.geoapi.feature;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.FeatureType;
 import org.opengis.feature.type.Name;
+
+import com.augtech.geoapi.geopackage.GeoPackage;
 
 /** An abstract class for loading and creating {@link SimpleFeature} from local storage, once 
  * they have been downloaded from the server. This class processes saved data in to a 
@@ -37,22 +44,139 @@ public abstract class FeatureLoader extends ArrayList<SimpleFeature> {
 	private static final long serialVersionUID = 4264941226040642321L;
 	protected Map<Name, SimpleFeatureType> definedFeatureTypes;
 	protected String nextID = "";
+	protected GeoPackage geoPackage = null;
+	protected int gpkgChunkSize = 50000;
+	protected boolean loadToGpkg = false;
+	protected int featuresInserted = 0;
+	private int featCounter = 0;
 
-	/**
+	/** Construct a new FeatureLoader that supports the supplied {@link FeatureType}'s 
+	 * and loads the data directly to the supplied GeoPackage<p>
+	 * FeatureType's are generally required where the underlying data being loaded
+	 * does not contain enough information to create a
+	 * proper type, for example where one ultimately wants 10 attributes, but the
+	 * data may only contain 2, or the data is an image.
 	 * 
-	 * @param featureTypes
+	 * @param featureTypes A Map of all the {@link SimpleFeatureType}'s that this
+	 * loader will use when building it's SimpleFeature's.
+	 * @param gpkg The GeoPackage to load feature's into. Note that tables are <i>not</i>
+	 * created automatically during load. If Null
+	 * the data is stored on this loader as per normal. The Default chunk size
+	 * is 25,000
+	 * @see #setLoadToGpkgOnly(int)
+	 */
+	public FeatureLoader(Map<Name, SimpleFeatureType> featureTypes, GeoPackage gpkg) {
+		this.definedFeatureTypes = featureTypes;
+		this.geoPackage = gpkg;
+		setLoadToGpkgOnly(25000);
+	}
+	
+	/** Construct a new FeatureLoader that supports the supplied {@link FeatureType}'s.<p>
+	 * FeatureType's are generally required where the underlying data being loaded
+	 * does not contain enough information to create a
+	 * proper type, for example where one ultimately wants 10 attributes, but the
+	 * data may only contain 2, or the data is an image.
+	 * 
+	 * @param featureTypes A Map of all the {@link SimpleFeatureType}'s that this
+	 * loader will use when building it's SimpleFeature's.
 	 */
 	public FeatureLoader(Map<Name, SimpleFeatureType> featureTypes) {
 		this.definedFeatureTypes = featureTypes;
 	}
-	
-	/** Processes an InputStream of data into this container.<p>
-	 * This process should handle uncompressing the stream if required.
-	 * 
-	 * @param inStream The InputStream to read features from
-	 */
-	public abstract int loadFeatures(InputStream inStream) throws Exception;
 
+	/** Processes an input file of data into this list backing this loader.<p>
+	 * 
+	 * @param inFile The File to read features from
+	 * @return The number of feature's loaded or inserted into the GeoPackage
+	 * @see #setLoadToGpkgOnly(int)
+	 */
+	public abstract int loadFeatures(File inFile) throws Exception;
+	
+	@Override
+	public void add(int index, SimpleFeature element) {
+		super.add(index, element);
+		featCounter++;
+		loadToGpkg();
+	}
+
+	@Override
+	public boolean add(SimpleFeature e) {
+		boolean ret = super.add(e);
+		featCounter++;
+		loadToGpkg();
+		return ret;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends SimpleFeature> c) {
+		boolean ret = super.addAll(c);
+		featCounter +=this.size();
+		loadToGpkg();
+		return ret;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends SimpleFeature> c) {
+		boolean ret= super.addAll(index, c);
+		featCounter +=this.size();
+		loadToGpkg();
+		return ret;
+	}
+	/**
+	 * 
+	 * @return
+	 * @throws IllegalDataException
+	 */
+	private boolean loadToGpkg() {
+		if (!loadToGpkg || geoPackage==null || featCounter < gpkgChunkSize) return false;
+
+		try {
+			featuresInserted += geoPackage.insertFeatures( this );
+			this.clear();
+			System.gc();
+			featCounter = 0;
+			
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+		
+		Logger.getAnonymousLogger().log(Level.INFO, "Inserted "+featuresInserted+" to GeoPackage");
+		return true;
+
+	}
+	/** Commit any final feature's stored on the loader to the GeoPackage.
+	 * This must be called at the end of {@link #loadFeatures(InputStream)}
+	 * to ensure all features are inserted in to the GeoPackage.<p>
+	 * Has no effect if no GeoPackage
+	 * 
+	 * @return The total number of features inserted in to the GeoPackage,
+	 * or -1 if no GeoPackage has been supplied.
+	 */
+	public final int commit() {
+		if (!loadToGpkg || geoPackage==null) return -1;
+		
+		try {
+			featuresInserted += geoPackage.insertFeatures( this );
+			this.clear();
+			System.gc();
+			featCounter = 0;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return featuresInserted;
+	}
+	/** Load the features directly in to a GeoPackage instead of this feature loader's
+	 * backing list. The backing list is cleared after processing in to the GeoPackage
+	 * to preserve memory.
+	 * 
+	 * @param chunkSize The number of features to process before inserting into
+	 * the GeoPackage. Defaults to 50,000 if < 0 is supplied
+	 */
+	public final void setLoadToGpkgOnly(int chunkSize) {
+		loadToGpkg = true;
+		gpkgChunkSize = chunkSize < 1 ? 50000 : chunkSize;
+	}
 	/** Get the {@link SimpleFeatureType}'s that were loaded
 	 * 
 	 * @return A Map of the loaded types, or null if nothing is/ was loaded
